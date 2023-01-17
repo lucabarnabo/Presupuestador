e4b3935ceace691d43ff93ddb6136003c5f05670
Luca Barnabo <lucabarnabo@yahoo.com.ar>
2023-01-17 15:02:01 -0300

    data

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
 import Navigation from "./Navigation";
+import Main from "./src/components/main";
 export default function App() {
   return (
-    /*  <View style={styles.container}>
-      <Text style={styles.titulo}>Login</Text>
-      <StatusBar style="auto" />
-    </View> */
-
-    <Navigation />
+    <Main />
+    /* <Navigation /> */
   );
 }
 
---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from 'react-native'
+import Constants from 'expo-constants'
+const Main = () => {
+    return (
+        <View style={{ marginTop: Constants.statusBarHeight }}>  
+            <Text>
+            hola facturador rapidito
+            </Text>
+    </View>
+
+)
+
+}
+
+export default Main
