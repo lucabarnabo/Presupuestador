dd062cd247baf84b3c0af39b1993582fd0684b05
Luca Barnabo <lucabarnabo@yahoo.com.ar>
2023-04-13 10:54:09 -0300

    Header andando

--- src/components/stepBar/StepHeader.js
+++ src/components/stepBar/StepHeader.js
@@ -28,6 +28,7 @@ const StepHeader = () => {
   const MAX_NUMBER_LINES = 2;
   const stepsContext = useContext(StepsContext);
   const { steps, currentStepIndex } = stepsContext;
+
   return (
     <View style={styles.container}>
       {steps.map((step, index) => (
@@ -48,3 +49,52 @@ const StepHeader = () => {
 };
 
 export default StepHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: Dimensions.get("window").width,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  stepContainer: {
+    flexDirection: "column",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleCircle: {
+    marginTop: 10,
+    fontSize: 12,
+    paddingBottom: 10,
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#2E81D3",
+    justifyContent: "center",
+    width: "85%",
+    position: "absolute",
+    top: 35,
+    marginHorizontal: 20,
+  },
+  circle: {
+    borderWidth: 1,
+    borderColor: "#2E81D3",
+    borderRadius: 50,
+    width: 30,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    zIndex: 1,
+  },
+  circleTitle: {
+    fontSize: 12,
+    color: "#fff",
+  },
+  selectedcircleTitle: {
+    fontSize: 12,
+    color: "#2E81D3",
+  },
+});
